d32938efbe68e2f6192dc46471f0c5d39ea9dfae
evandro-ribeiro <evandrormf.dev@gmail.com>
2024-09-02 14:30:04 -0300

    adicionando projeto

---
+++ README.md
@@ -0,0 +1,12 @@
+# WikiRepos - Busca e Listagem de Repositórios GitHub
+
+<img width=100% src="./src/assets/banner.png"/>
+
+## Contexto
+
+Projeto React de listagem de repositórios utilizando a busca destes repositórios através da API do GitHub.
+
+## Libs utilizadas
+
+- Styled Components
+- Axios
---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:20px;
+    }
+
+    a {
+        color: violet;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+
+    .remover {
+        background-color: blue;
+        padding: 8px;
+        border: 2px solid darkblue;
+        border-radius: 5px;
+        margin-top: 20px;
+        color: white;
+        cursor: pointer;
+    }
+`
